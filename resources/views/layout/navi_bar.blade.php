ff31a3ba34461a64614878999dc5e0b3a39b4753
nlyu <lnc0519@gmail.com>
2017-03-13 19:37:23 -0500

    fix css add gender registraton
    fix css & add gender

--- resources/views/layout/navi_bar.blade.php
+++ resources/views/layout/navi_bar.blade.php
@@ -99,69 +99,53 @@
                      <br>
                      <input type="submit" value="Sign Up" />
                     </form> -->
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                    <form  id = "user_register" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name">Name</label>
+                            <input id="name" type="firstname"  name="name" value="{{ old('name') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('name'))
+                            <span class="help-block">
+                            <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                            @endif
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                            <label for="email">E-Mail Address</label>
+                                <input id="email" type="email" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
+                            <label for="password">Password</label>
+                                <input id="password" type="password" name="password" required>
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                            <label for="password-confirm">Confirm Password</label>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                         </div>
 
                         <label name="age">Age</label>
                         <input type="age" name="age"/>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
+                        <input type="radio" name="gender" value="0"> Male
+                        <input type="radio" name="gender" value="1"> Female<br>
+                        <br>
+                        <input type="submit" value="Sign Up" />
                     </form>
                    </fieldset>
                 </div>
